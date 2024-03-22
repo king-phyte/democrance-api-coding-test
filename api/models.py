14e078806bccd5885033f5f66cbee569c734bac7
King Phyte <kofiasante1400@gmail.com>
2024-03-22 12:40:57 +0000

    feat: add quote model and migrate

--- api/models.py
+++ api/models.py
@@ -37,3 +37,44 @@ class Customer(models.Model):
             "last_name": self.last_name,
             "dob": self.date_of_birth.strftime("%d-%m-%Y"),
         }
+
+
+class Quote(models.Model):
+    class QuoteStatus(models.TextChoices):
+        NEW = "new"
+        ACCEPTED = "accepted"
+        REJECTED = "rejected"
+        ACTIVE = "active"
+
+    class QuoteType(models.TextChoices):
+        PERSONAL_ACCIDENT = "personal-accident"
+        HOMEOWNER_INSURANCE = "homeowner-insurance"
+        AUTO_INSURANCE = "auto"
+
+    id = models.BigAutoField(primary_key=True)
+    status = models.CharField(
+        max_length=20,
+        choices=QuoteStatus,
+        default=QuoteStatus.NEW,
+    )
+    type = models.CharField(max_length=20, null=False, blank=False, choices=QuoteType)
+    premium = models.DecimalField(max_digits=8, decimal_places=2)
+    cover = models.DecimalField(max_digits=10, decimal_places=2)
+
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "quotes"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "status": self.status,
+            "type": self.type,
+            "premium": self.premium,
+            "cover": self.cover,
+            "customer": self.customer.serialize(),
+        }
