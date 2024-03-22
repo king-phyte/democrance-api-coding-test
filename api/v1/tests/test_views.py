e5f1f6dc869f359e2e275b3e2aaf0c49d1bccbf1
King Phyte <kofiasante1400@gmail.com>
2024-03-22 12:41:54 +0000

    test: add tests for quote functionality

--- api/v1/tests/test_views.py
+++ api/v1/tests/test_views.py
@@ -1,8 +1,12 @@
+import datetime
+
 from django.test import Client, TestCase
 from django.urls import reverse_lazy
 
+from api.models import Customer, Quote
 
-class APITestCase(TestCase):
+
+class CustomerTestCase(TestCase):
     # create_customer_url = reverse("api:v1:create_customer")
     create_customer_url = reverse_lazy("api:v1:create-customer")
 
@@ -45,3 +49,96 @@ class APITestCase(TestCase):
 
         self.assertEqual(response.status_code, 422)
         self.assertEqual(response.json()["detail"]["dob"][0]["code"], "invalid")
+
+
+class QuoteTestCase(TestCase):
+    def setUp(self):
+
+        self.customer = Customer.objects.create(
+            first_name="Ben",
+            last_name="Stokes",
+            date_of_birth=datetime.date(year=1991, month=6, day=25),
+        )
+        self.quote = Quote.objects.create(
+            customer=self.customer,
+            cover=20000,
+            premium=200,
+            status=Quote.QuoteStatus.NEW,
+            type=Quote.QuoteType.PERSONAL_ACCIDENT,
+        )
+
+    def test_create_quote(self):
+        response = self.client.post(
+            "/api/v1/quotes/",
+            {
+                "customer_id": self.customer.id,
+                "type": "personal-accident",
+            },
+            content_type="application/json",
+        )
+
+        self.assertEqual(response.status_code, 201)
+
+        for key in {"id", "customer", "cover", "premium"}:
+            self.assertIn(key, response.json())
+
+        self.assertEqual(response.json()["customer"]["id"], self.customer.id)
+
+        # According to the assumption made, people within the ages of 25 and 50 get their
+        # cover multiplied by 1.1 and premium by 1.5
+        # See :method:`api.v1.forms.QuoteCreationForm.save`
+        self.assertEqual(response.json()["cover"], 20000 * 1.1)
+        self.assertEqual(response.json()["premium"], 200 * 1.5)
+
+    def test_create_quote_with_invalid_customer_id(self):
+        response = self.client.post(
+            "/api/v1/quotes/",
+            {
+                "customer_id": 9999,
+                "type": "personal-accident",
+            },
+            content_type="application/json",
+        )
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual("customer not found", response.json()["detail"])
+
+    def test_accept_quote(self):
+        response = self.client.put(
+            f"/api/v1/quotes/{self.quote.id}/status/",
+            {"status": "accepted"},
+            content_type="application/json",
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["status"], "accepted")
+
+    def test_pay_for_quote(self):
+        response = self.client.put(
+            f"/api/v1/quotes/{self.quote.id}/status/",
+            {"status": "active"},
+            content_type="application/json",
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["status"], "active")
+
+    def test_update_nonexistent_quote(self):
+        response = self.client.put(
+            f"/api/v1/quotes/99999/status/",
+            {"status": "accepted"},
+            content_type="application/json",
+        )
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json()["detail"], "quote not found")
+
+    def test_update_quote_with_invalid_status(self):
+        response = self.client.put(
+            f"/api/v1/quotes/{self.quote.id}/status/",
+            {"status": "something"},
+            content_type="application/json",
+        )
+
+        self.assertEqual(response.status_code, 422)
+        self.assertIn("invalid quote status", response.json()["detail"])
